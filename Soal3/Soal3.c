2a4702bd028c124be09bfa52902e53bb36fe86b4
Clever Dicki <48941105+cleverdicki@users.noreply.github.com>
2020-03-13 01:24:38 +0700

    Update Soal3.c

--- Soal3/Soal3.c
+++ Soal3/Soal3.c
@@ -5,12 +5,14 @@
 #include <dirent.h>
 #include <stdio.h>
 #include <string.h>
+#include <sys/stat.h>
 
 int main() {
   pid_t child_id1, child_id2, child_id3, child_id4, child_id5, child_id6, child_id7, child_id8;
   int status;
   char ch[100];
   FILE *destination;
+  struct stat check;
 
   child_id1 = fork();
 
@@ -21,8 +23,8 @@ int main() {
       if (child_id3 == 0) {
         child_id4 = fork();
         if (child_id4 == 0) {
-          child_id5 = fork();
-          if (child_id5 == 0) {
+          // child_id5 = fork();
+          // if (child_id5 == 0) {
             char *argv[] = {"mkdir", "-p", "/home/dicki/modul2/indomie", NULL};
             execv("/bin/mkdir", argv);
           }else {
@@ -38,48 +40,53 @@ int main() {
         }
       }else {
         while ((wait(&status)) > 0);
-        char *argv[] = {"bash", "mv.sh" , NULL};
-        execv("/bin/bash", argv);
+        sprintf(ch, "/home/dicki/Praktikum/Praktikum2/jpg");
+        char *argv[] = {"mv", ch, "/home/dicki/modul2", NULL};
+        execv("/bin/mv", argv);
       }  
     }else {
       while ((wait(&status)) > 0);
-      char *argv[] = {"bash", "mvfile.sh" , NULL};
-      execv("/bin/bash", argv);
-    }
-  }else {
-    while ((wait(&status)) > 0);
-    chdir("/home/dicki/modul2/jpg/");
-    struct dirent *de;
-    DIR *dr = opendir(".");
-    while ((de = readdir(dr)) != NULL){
-      if (strcmp(de->d_name, ".") == 0 || strcmp(de->d_name, "..") == 0)
-        continue;
-      child_id6 = fork();
-      if (child_id6 == 0) {
-        child_id7 = fork();
-        if (child_id7 == 0) {
-          child_id8 = fork();
-          if (child_id8 == 0) {
-            sprintf(ch, "/home/dicki/modul2/jpg/%s", de->d_name);
-            char *argv[] = {"mv", ch, "/home/dicki/modul2/indomie/", NULL};
-            execv("/bin/mv", argv);
+      chdir("/home/dicki/modul2/jpg/");
+      struct dirent *de;
+      DIR *dr = opendir(".");
+      while ((de = readdir(dr)) != NULL){
+        if (strcmp(de->d_name, ".") == 0 || strcmp(de->d_name, "..") == 0)
+          continue;
+        child_id6 = fork();
+        if (child_id6 == 0) {
+          child_id7 = fork();
+          if (child_id7 == 0) {
+            child_id8 = fork();
+            if (child_id8 == 0) {
+              if (stat(de->d_name,&check) == 0){
+                if (check.st_mode & S_IFDIR) {
+                  sprintf(ch, "/home/dicki/modul2/jpg/%s", de->d_name);
+                  char *argv[] = {"mv", ch, "/home/dicki/modul2/indomie", NULL};
+                  execv("/bin/mv", argv);
+                }else {
+                  while ((wait(&status)) > 0);
+                  sprintf(ch, "/home/dicki/modul2/jpg/%s", de->d_name);
+                  char *argv[] = {"mv", ch, "/home/dicki/modul2/sedaap" , NULL};
+                  execv("/bin/mv", argv);
+                } 
+              }
+            }else {
+                while ((wait(&status)) > 0);
+                sprintf(ch, "/home/dicki/modul2/indomie/%s/coba1.txt", de->d_name);
+                destination = fopen(ch, "w+");
+                fclose(destination);
+            }
           }else {
             while ((wait(&status)) > 0);
-            sprintf(ch, "/home/dicki/modul2/indomie/%s/coba1.txt", de->d_name);
+            sleep(3);
+            sprintf(ch, "/home/dicki/modul2/indomie/%s/coba2.txt", de->d_name);
             destination = fopen(ch, "w+");
             fclose(destination);
           }
         }else {
-          while ((wait(&status)) > 0);
-          sleep(3);
-          sprintf(ch, "/home/dicki/modul2/indomie/%s/coba2.txt", de->d_name);
-          destination = fopen(ch, "w+");
-          fclose(destination);
-          exit(0);
-        } 
-      }else {
-        while ((wait(&status)) > 0);        
+          while ((wait(&status)) > 0);    
+        }
       }
+      close(dr);
     }
-  }  
 }
